0f35f299d37a2fafc2e801b231868ba83446e654
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-07-09 19:56:37 +0600

    feat: Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
 	const [darkMode, setDarkMode] = useState(false)
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
 	const toggleDarkMode = () => {
 		setDarkMode(!darkMode)
 		
 	}
+
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen)
+	}
+
 	return (
 		<div className={`${darkMode && "dark"} font-quicksand`}>
-			<Header toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+			<Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} toggleSidebar={toggleSidebar}/>
+			<Sidebar isSidebarOpen={isSidebarOpen}/>
 		</div>
 	);
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { FaMoon, FaSun } from "react-icons/fa"
 import { HiOutlineMenuAlt1 } from "react-icons/hi"
 import { MdSpaceDashboard } from "react-icons/md"
+import PropTypes from "prop-types";
 
-const Header = ({ darkMode, toggleDarkMode}) => {
+const Header = ({ darkMode, toggleDarkMode, toggleSidebar}) => {
   return (
 	<nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:text-white">
 		<div className="px-3 py-3 lg:px-5 lg:pl-3">
 			<div className="flex items-center justify-between">
 				<div className="flex items-center justify-start rtl:justify-end">
-					<button className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+					<button className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+					onClick={toggleSidebar}
+					>
 						<HiOutlineMenuAlt1 className="text-2xl" />
 					</button>
 					<a href="#" className="flex items-center ms-4 md:me-24">
@@ -25,5 +28,11 @@ const Header = ({ darkMode, toggleDarkMode}) => {
 	</nav>
   )
 }
+
+Header.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  toggleDarkMode: PropTypes.func.isRequired,
+  toggleSidebar: PropTypes.func.isRequired
+};
 
 export default Header
